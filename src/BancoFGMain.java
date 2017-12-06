fd6b195495491bdf167ea39c8827b5233c1d1437
rodrigo.monterio <rodrigo.monterio@RECDSKFS016223.linx-inves.com.br>
2017-12-06 14:16:07 -0300

    CR #003 Apenas uma conta está adicionada.

--- src/BancoFGMain.java
+++ src/BancoFGMain.java
@@ -1,3 +1,5 @@
+//Projeto do Professor Endrigo - Faculdade dos Guararapes.
+
 
 public class BancoFGMain {	
 
@@ -5,7 +7,11 @@ public class BancoFGMain {
 		RepositorioContasArray contas = new RepositorioContasArray(3);
 		
 		Cliente c1 = new Cliente();
-		Conta conta2 = new Conta("1", 100, c1);
+		Conta conta1 = new Conta("1", 100, c1);
+		
+		Cliente c2 = new Cliente();
+		
+		Conta conta2 = new Conta("2", 200, c2);
 	}
 
 }
